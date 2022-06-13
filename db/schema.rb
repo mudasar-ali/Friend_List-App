6c18fc04038aebb0b444568adaee7853202ad9a1
mudassar-ali <mudassar.ali@devsinc.com>
2022-06-13 21:08:00 +0500

    authorized user can edit or delete their friends

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_13_121834) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_13_141427) do
   create_table "friends", force: :cascade do |t|
     t.string "firs_name"
     t.string "last_name"
@@ -19,6 +19,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_13_121834) do
     t.string "twitter"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_friends_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
